52d6e25c0770c64a3c09e28ff67a6bc214b09254
Julien Falxa <jfalxa.sta@gmail.com>
2019-03-05 16:32:18 +0100

    Revert "react-systyle: deal with refs properly"
    This reverts commit 2990088eda64ce6b1c6606fb3b7b49e871e93317.

--- packages/react-systyle/src/index.tsx
+++ packages/react-systyle/src/index.tsx
@@ -6,35 +6,22 @@ import { jsx, ThemeContext } from '@emotion/core'
 
 const uid = () => Math.random().toString(36).slice(2) // prettier-ignore
 
-const builder = (moulinette: Function) => {
+const builder = (moulinette: Function) =>
   class Styled extends React.PureComponent<any, any> {
     static contextType = ThemeContext
+    static className = `s${uid()}`
+    static toString = () => `.${Styled.className}`
 
     render() {
-      const {
-        as: Type = 'div',
-        css = null,
-        theme: _ = null,
-        fwdRef = null,
-        ...props
-      } = moulinette({ ...this.props, theme: this.context }) || {}
+      const { as: Type = 'div', css = null, theme: _ = null, ...props } =
+        moulinette({ ...this.props, theme: this.context }) || {}
 
       const className = props.className
-        ? [RefStyled.className, props.className].join(' ')
-        : RefStyled.className
+        ? [Styled.className, props.className].join(' ')
+        : Styled.className
 
-      return <Type {...props} ref={fwdRef} className={className} css={css} />
+      return <Type {...props} className={className} css={css} />
     }
   }
-
-  const RefStyled: any = React.forwardRef((props, ref) => (
-    <Styled {...props} fwdRef={ref} />
-  ))
-
-  RefStyled.className = `s${uid()}`
-  RefStyled.toString = () => `.${RefStyled.className}`
-
-  return RefStyled
-}
 
 export default createStyled(builder)
